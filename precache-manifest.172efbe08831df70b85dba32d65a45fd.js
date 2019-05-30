57f2056d3d7da4c5956391d0e41336d43b14be55
Philippe Weier <philippe.weier@epfl.ch>
2019-05-30 18:32:26 +0200

    Updates

---
+++ precache-manifest.172efbe08831df70b85dba32d65a45fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e6aa7830399c8a53c9fdd6320d8255",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "738d84c9079a996ee1a4",
+    "url": "/blog/static/js/2.a73a77c8.chunk.js"
+  },
+  {
+    "revision": "a4db1396e68eed0e2a86",
+    "url": "/blog/static/js/main.146a9cf7.chunk.js"
+  },
+  {
+    "revision": "eeecc51c3b4e3b0a67a2",
+    "url": "/blog/static/js/runtime~main.516a48d0.js"
+  },
+  {
+    "revision": "bb34f7ecb5a3f9748b009eea56d102af",
+    "url": "/blog/static/media/lotrScreenshot.bb34f7ec.png"
+  },
+  {
+    "revision": "bbd436f55e4a09e14af65432563e7f1c",
+    "url": "/blog/static/media/phil_picture.bbd436f5.jpg"
+  },
+  {
+    "revision": "961401b65829ebc2b56eb6aa7e0796cd",
+    "url": "/blog/static/media/qulkanScreenshot.961401b6.png"
+  }
+]);
