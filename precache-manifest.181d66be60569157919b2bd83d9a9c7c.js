5a88d919e8d40208863d4b93ae0a9cbb20bfc639
Philippe Weier <philippe.weier@epfl.ch>
2019-05-30 18:36:51 +0200

    Updates

---
+++ precache-manifest.181d66be60569157919b2bd83d9a9c7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faaa5324c2cfb56feeacd9259aea3dbe",
+    "url": "/portefolio/index.html"
+  },
+  {
+    "revision": "738d84c9079a996ee1a4",
+    "url": "/portefolio/static/js/2.a73a77c8.chunk.js"
+  },
+  {
+    "revision": "a4db1396e68eed0e2a86",
+    "url": "/portefolio/static/js/main.146a9cf7.chunk.js"
+  },
+  {
+    "revision": "478c0f91fb2f52d36209",
+    "url": "/portefolio/static/js/runtime~main.32a7b7e9.js"
+  },
+  {
+    "revision": "bb34f7ecb5a3f9748b009eea56d102af",
+    "url": "/portefolio/static/media/lotrScreenshot.bb34f7ec.png"
+  },
+  {
+    "revision": "bbd436f55e4a09e14af65432563e7f1c",
+    "url": "/portefolio/static/media/phil_picture.bbd436f5.jpg"
+  },
+  {
+    "revision": "961401b65829ebc2b56eb6aa7e0796cd",
+    "url": "/portefolio/static/media/qulkanScreenshot.961401b6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog/precache-manifest.172efbe08831df70b85dba32d65a45fd.js"
+  "/portefolio/precache-manifest.181d66be60569157919b2bd83d9a9c7c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portefolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
