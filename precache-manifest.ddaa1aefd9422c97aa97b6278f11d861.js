d0c329f52cfb58f17a75cd30e07a14488e5f3f79
Philippe Weier <philippe.weier@epfl.ch>
2019-06-10 14:33:44 +0200

    Updates

---
+++ precache-manifest.ddaa1aefd9422c97aa97b6278f11d861.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6381b02276725fdefc6b0c104046ba38",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a916fc03a924601c4401",
+    "url": "/portfolio/static/js/2.7a2c647d.chunk.js"
+  },
+  {
+    "revision": "bfc11f5641dcb80c33d0",
+    "url": "/portfolio/static/js/main.c9504894.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "e6e1053fb2e67410d6b5de2aeddceb65",
+    "url": "/portfolio/static/media/cs440.e6e1053f.png"
+  },
+  {
+    "revision": "abcda57d3d1c9e12595739ea91b84568",
+    "url": "/portfolio/static/media/layered_aniso.abcda57d.png"
+  },
+  {
+    "revision": "bb34f7ecb5a3f9748b009eea56d102af",
+    "url": "/portfolio/static/media/lotrScreenshot.bb34f7ec.png"
+  },
+  {
+    "revision": "bbd436f55e4a09e14af65432563e7f1c",
+    "url": "/portfolio/static/media/phil_picture.bbd436f5.jpg"
+  },
+  {
+    "revision": "f3555398ea17face56e5756c44f2a049",
+    "url": "/portfolio/static/media/procaryotaScreenshot.f3555398.png"
+  },
+  {
+    "revision": "566af88072294cdcafcb206c39dd34f3",
+    "url": "/portfolio/static/media/qulkanScreenshot.566af880.png"
+  },
+  {
+    "revision": "87d265b9169524b8933fd3e0fc4419cb",
+    "url": "/portfolio/static/media/resume.87d265b9.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portefolio/precache-manifest.285d5b5d3d4bd681ae31540fd7ea77a9.js"
+  "/portfolio/precache-manifest.ddaa1aefd9422c97aa97b6278f11d861.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portefolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
