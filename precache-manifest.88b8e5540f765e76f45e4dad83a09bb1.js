98ce7cc454a9987da23f8a94811c01fe7f7d806d
Philippe Weier <philippe.weier@epfl.ch>
2021-06-25 15:10:21 +1200

    Updates

---
+++ precache-manifest.88b8e5540f765e76f45e4dad83a09bb1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1a67895612b8948080283f9b076533",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "24bf4c0d4092627c0af6",
+    "url": "/portfolio/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "24bf4c0d4092627c0af6",
+    "url": "/portfolio/static/js/2.b165b85d.chunk.js"
+  },
+  {
+    "revision": "19e4219b6747b6313c40",
+    "url": "/portfolio/static/js/main.9e6ca451.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "e6e1053fb2e67410d6b5de2aeddceb65",
+    "url": "/portfolio/static/media/cs440.e6e1053f.png"
+  },
+  {
+    "revision": "28013ed9fa1abd79a2cbf1033a64d250",
+    "url": "/portfolio/static/media/grazing_angles_overblur.28013ed9.png"
+  },
+  {
+    "revision": "7493018c325b8619cbe889e6797d1b9c",
+    "url": "/portfolio/static/media/imageAndVideoProc.7493018c.gif"
+  },
+  {
+    "revision": "20ca472ba352abfa4bceda674112229e",
+    "url": "/portfolio/static/media/layered_aniso.20ca472b.png"
+  },
+  {
+    "revision": "3b40021847781153b61277daf4ca53fa",
+    "url": "/portfolio/static/media/layered_aniso_05_05_top_varying.3b400218.png"
+  },
+  {
+    "revision": "bb34f7ecb5a3f9748b009eea56d102af",
+    "url": "/portfolio/static/media/lotrScreenshot.bb34f7ec.png"
+  },
+  {
+    "revision": "bbd436f55e4a09e14af65432563e7f1c",
+    "url": "/portfolio/static/media/phil_picture.bbd436f5.jpg"
+  },
+  {
+    "revision": "f3555398ea17face56e5756c44f2a049",
+    "url": "/portfolio/static/media/procaryotaScreenshot.f3555398.png"
+  },
+  {
+    "revision": "566af88072294cdcafcb206c39dd34f3",
+    "url": "/portfolio/static/media/qulkanScreenshot.566af880.png"
+  },
+  {
+    "revision": "02a7f1c6534b331affe1395de5cdb561",
+    "url": "/portfolio/static/media/resume.02a7f1c6.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09a63e6e5c8487aa9cafafc1f239a7a7.js"
+  "/portfolio/precache-manifest.88b8e5540f765e76f45e4dad83a09bb1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
