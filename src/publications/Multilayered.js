4b622c3b5c4272cb226b587caaa534a27f5b0ee4
Philippe Weier <philippe.weier@epfl.ch>
2021-09-02 20:14:40 +0200

    some more formatting

--- src/publications/Multilayered.js
+++ src/publications/Multilayered.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { makeStyles, withStyles, useTheme } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import { Grid, Link, Button, Box, Container } from '@material-ui/core';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import ArchiveIcon from '@material-ui/icons/Archive';
-import DescriptionIcon from '@material-ui/icons/Description';
+import React from "react";
+import { makeStyles, withStyles, useTheme } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { Grid, Link, Button, Box, Container } from "@material-ui/core";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import ArchiveIcon from "@material-ui/icons/Archive";
+import DescriptionIcon from "@material-ui/icons/Description";
 
-import Footer from '../components/Footer'
+import Footer from "../components/Footer";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   content: {
     height: "100%",
     backgroundColor: theme.palette.grey[100],
@@ -23,11 +23,11 @@ const useStyles = makeStyles(theme => ({
     padding: 50,
     paddingTop: 60,
     paddingBottom: 50,
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       paddingTop: 30,
       padding: 20,
     },
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       paddingLeft: 150,
       paddingRight: 150,
     },
@@ -36,15 +36,14 @@ const useStyles = makeStyles(theme => ({
     marginTop: 30,
     height: 450,
     width: 800,
-    [theme.breakpoints.down('md')]: {
+    [theme.breakpoints.down("md")]: {
       height: 300,
       width: 600,
     },
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       width: "100%",
       height: 225,
     },
-
   },
   image: {
     height: "100%",
@@ -55,7 +54,7 @@ const useStyles = makeStyles(theme => ({
     marginBottom: 30,
     height: 450,
     width: 450,
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       width: "100%",
       height: 300,
     },
@@ -65,7 +64,7 @@ const useStyles = makeStyles(theme => ({
     marginBottom: 30,
     height: 600,
     width: 600,
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       width: "100%",
       height: 300,
     },
@@ -79,61 +78,79 @@ const useStyles = makeStyles(theme => ({
   },
   figureLegend: {
     maxWidth: "70%",
-    [theme.breakpoints.down('sm')]: {
-      maxWidth: "100%"
+    [theme.breakpoints.down("sm")]: {
+      maxWidth: "100%",
     },
-  }
+  },
 }));
 
-const StyledLink = withStyles(theme => ({
+const StyledLink = withStyles((theme) => ({
   root: {
-    '&:hover': {
+    "&:hover": {
       color: "#af7b6b",
     },
-  }
-}))(props => <Link underline="none" {...props} />);
-
+  },
+}))((props) => <Link underline="none" {...props} />);
 
 function Multilayered(props) {
   window.scrollTo(0, 0);
-  
+
   const theme = useTheme();
   const classes = useStyles();
 
-  const smallWidth = useMediaQuery(theme.breakpoints.down('sm'));
+  const smallWidth = useMediaQuery(theme.breakpoints.down("sm"));
 
-  const [paperLink, paperLinkLabel] = ["http://jcgt.org/published/0009/02/03/paper.pdf", "Paper (10.8 MiB)"];
-  const archiveDatas = [["http://jcgt.org/published/0009/02/03/mitsuba_supplemental.zip", "Code (20.8 KiB)"], ["http://jcgt.org/published/0009/02/03/html_supplemental.zip", "Supplemental (4.1 GiB)"]];
-
+  const [paperLink, paperLinkLabel] = [
+    "http://jcgt.org/published/0009/02/03/paper.pdf",
+    "Paper (10.8 MiB)",
+  ];
+  const archiveDatas = [
+    [
+      "http://jcgt.org/published/0009/02/03/mitsuba_supplemental.zip",
+      "Code (20.8 KiB)",
+    ],
+    [
+      "http://jcgt.org/published/0009/02/03/html_supplemental.zip",
+      "Supplemental (4.1 GiB)",
+    ],
+  ];
 
   return (
     <Container className={classes.content}>
       <div className={classes.root}>
-
         <Grid
           container
           direction="column"
           justify="flex-start"
           alignItems={smallWidth ? "flex-start" : "center"}
         >
-
-          <Grid item  >
-
+          <Grid item>
             <Typography variant="h5" gutterBottom>
-              <Box fontWeight="fontWeightRegular">Rendering Layered Materials with Anisotropic Interfaces</Box>
+              <Box fontWeight="fontWeightRegular">
+                Rendering Layered Materials with Anisotropic Interfaces
+              </Box>
             </Typography>
 
             <Typography>
-              <StyledLink href={process.env.PUBLIC_URL + "/"}>Philippe Weier</StyledLink> and <StyledLink href="https://belcour.github.io/blog/">Laurent Belcour</StyledLink>, 2020
+              <StyledLink href={process.env.PUBLIC_URL + "/"}>
+                Philippe Weier
+              </StyledLink>{" "}
+              and{" "}
+              <StyledLink href="https://belcour.github.io/blog/">
+                Laurent Belcour
+              </StyledLink>
+              , 2020
             </Typography>
 
             <Typography gutterBottom>
-              Published in Journal of Computer Graphics Techniques (<StyledLink href="http://jcgt.org/">JCGT</StyledLink>)
+              Published in Journal of Computer Graphics Techniques (
+              <StyledLink href="http://jcgt.org/">JCGT</StyledLink>)
             </Typography>
 
             <Card className={classes.card}>
-              <CardMedia className={classes.image}
-                image={require('../data/layered/layered_aniso.png')}
+              <CardMedia
+                className={classes.image}
+                image={require("../data/layered/layered_aniso.png")}
                 title="Layered Teaser"
               />
             </Card>
@@ -145,38 +162,68 @@ function Multilayered(props) {
                 justify="flex-start"
                 alignItems="center"
               >
-
-                <Grid item  >
-                  <Button variant="outlined" color="secondary" className={classes.button} target="_blank" href={paperLink}><DescriptionIcon className={classes.icon} />
+                <Grid item>
+                  <Button
+                    variant="outlined"
+                    color="secondary"
+                    className={classes.button}
+                    target="_blank"
+                    href={paperLink}
+                  >
+                    <DescriptionIcon className={classes.icon} />
                     {paperLinkLabel}
                   </Button>
                 </Grid>
 
-                {archiveDatas.map(([archiveLink, archiveLinkLabel],) => (
+                {archiveDatas.map(([archiveLink, archiveLinkLabel]) => (
                   <Grid item>
-                    <Button variant="outlined" color="secondary" className={classes.button} target="_blank" href={archiveLink}><ArchiveIcon className={classes.icon} />
+                    <Button
+                      variant="outlined"
+                      color="secondary"
+                      className={classes.button}
+                      target="_blank"
+                      href={archiveLink}
+                    >
+                      <ArchiveIcon className={classes.icon} />
                       {archiveLinkLabel}
                     </Button>
                   </Grid>
                 ))}
-
               </Grid>
 
-
-              <Grid item style={smallWidth ? { marginTop: 20 } : { marginTop: 40 }} >
+              <Grid
+                item
+                style={smallWidth ? { marginTop: 20 } : { marginTop: 40 }}
+              >
                 <Typography variant="h6" gutterBottom>
                   <Box fontWeight={500}>Abstract</Box>
                 </Typography>
                 <Typography variant="body1" gutterBottom>
-                  We present a lightweight and efficient method to render layered materials with anisotropic interfaces. Our work extends the statistical framework of <StyledLink href="https://belcour.github.io/blog/research/2018/05/05/brdf-realtime-layered.html">Belcour [2018]</StyledLink> to handle anisotropic microfacet models. A key insight to our work is that when projected on the tangent plane, BRDF lobes from an anisotropic GGX distribution are well approximated by ellipsoidal distributions aligned with the tangent frame: its covariance matrix is diagonal in this space. We leverage this property and perform the adding-doubling algorithm on each anisotropy axis independently. We further update the mapping of roughness to directional variance and the evaluation of the average reflectance to account for anisotropy. We extensively tested this model against ground truth.
+                  We present a lightweight and efficient method to render
+                  layered materials with anisotropic interfaces. Our work
+                  extends the statistical framework of{" "}
+                  <StyledLink href="https://belcour.github.io/blog/research/2018/05/05/brdf-realtime-layered.html">
+                    Belcour [2018]
+                  </StyledLink>{" "}
+                  to handle anisotropic microfacet models. A key insight to our
+                  work is that when projected on the tangent plane, BRDF lobes
+                  from an anisotropic GGX distribution are well approximated by
+                  ellipsoidal distributions aligned with the tangent frame: its
+                  covariance matrix is diagonal in this space. We leverage this
+                  property and perform the adding-doubling algorithm on each
+                  anisotropy axis independently. We further update the mapping
+                  of roughness to directional variance and the evaluation of the
+                  average reflectance to account for anisotropy. We extensively
+                  tested this model against ground truth.
                 </Typography>
-
 
                 <Typography variant="h6" gutterBottom style={{ marginTop: 10 }}>
                   <Box fontWeight={500}>Validation</Box>
                 </Typography>
                 <Typography variant="body1" gutterBottom>
-                    We validated that our model was visualy close to the ground truth. See our supplemental material for all our results (warning: the archive is > 4GB).
+                  We validated that our model was visualy close to the ground
+                  truth. See our supplemental material for all our results
+                  (warning: the archive is > 4GB).
                 </Typography>
               </Grid>
               <Grid
@@ -185,51 +232,56 @@ function Multilayered(props) {
                 justify="center"
                 alignItems="center"
               >
-
-                <Card className={classes.mainComparCard} style={{ border: "none", boxShadow: "none" }}>
-                  <CardMedia className={classes.image}
-                    image={require('../data/layered/layered_aniso_05_05_top_varying.png')}
+                <Card
+                  className={classes.mainComparCard}
+                  style={{ border: "none", boxShadow: "none" }}
+                >
+                  <CardMedia
+                    className={classes.image}
+                    image={require("../data/layered/layered_aniso_05_05_top_varying.png")}
                     title="layered_aniso_05_05_top_varying"
                   />
                 </Card>
-                <Typography variant="body1" gutterBottom className={classes.figureLegend}>
+                <Typography
+                  variant="body1"
+                  gutterBottom
+                  className={classes.figureLegend}
+                >
                   <i>
-                    Example of the validation of our layered model with respect to the ground truth when changing the roughness of the top dielectric layer.
+                    Example of the validation of our layered model with respect
+                    to the ground truth when changing the roughness of the top
+                    dielectric layer.
                   </i>
                 </Typography>
 
-
-                <Card className={classes.grazingCard} >
-                  <CardMedia className={classes.image}
-                    image={require('../data/layered/grazing_angles_overblur.png')}
+                <Card className={classes.grazingCard}>
+                  <CardMedia
+                    className={classes.image}
+                    image={require("../data/layered/grazing_angles_overblur.png")}
                     title="layered_aniso_05_05_top_varying"
                   />
                 </Card>
-                <Typography variant="body1" gutterBottom className={classes.figureLegend}>
+                <Typography
+                  variant="body1"
+                  gutterBottom
+                  className={classes.figureLegend}
+                >
                   <i>
-                    Our method differs from the reference at grazing angles. This is due to the correlation between total internal reflection (TIR) and our precomputed average reflectance for one bounce. Since we do not account for it, our model overblurs the reflectance at grazinangles.
+                    Our method differs from the reference at grazing angles.
+                    This is due to the correlation between total internal
+                    reflection (TIR) and our precomputed average reflectance for
+                    one bounce. Since we do not account for it, our model
+                    overblurs the reflectance at grazinangles.
                   </i>
                 </Typography>
-
               </Grid>
-
-
-
             </Grid>
-
-
           </Grid>
-
-
-
-        </Grid >
-
-      </div >
-    <Footer />
-
+        </Grid>
+      </div>
+      <Footer />
     </Container>
   );
-
 }
 
 export default Multilayered;
--- src/publications/Opsr.js
+++ src/publications/Opsr.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { makeStyles, withStyles, useTheme } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import { Grid, Link, Button, Box, Container } from '@material-ui/core';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import DescriptionIcon from '@material-ui/icons/Description';
-import ArchiveIcon from '@material-ui/icons/Archive';
-import SlideshowIcon from '@material-ui/icons/Slideshow';
-import ReactPlayer from 'react-player'
+import React from "react";
+import { makeStyles, withStyles, useTheme } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { Grid, Link, Button, Box, Container } from "@material-ui/core";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ArchiveIcon from "@material-ui/icons/Archive";
+import SlideshowIcon from "@material-ui/icons/Slideshow";
+import ReactPlayer from "react-player";
 
-import Footer from '../components/Footer'
+import Footer from "../components/Footer";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   content: {
     height: "100%",
     backgroundColor: theme.palette.grey[100],
@@ -25,11 +25,11 @@ const useStyles = makeStyles(theme => ({
     padding: 50,
     paddingTop: 60,
     paddingBottom: 50,
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       paddingTop: 30,
       padding: 20,
     },
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       paddingLeft: 150,
       paddingRight: 150,
     },
@@ -38,15 +38,14 @@ const useStyles = makeStyles(theme => ({
     marginTop: 30,
     height: 450,
     width: 800,
-    [theme.breakpoints.down('md')]: {
+    [theme.breakpoints.down("md")]: {
       height: 300,
       width: 600,
     },
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       width: "100%",
       height: 225,
     },
-
   },
   image: {
     height: "100%",
@@ -57,7 +56,7 @@ const useStyles = makeStyles(theme => ({
     marginBottom: 30,
     height: 450,
     width: 450,
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       width: "100%",
       height: 300,
     },
@@ -67,7 +66,7 @@ const useStyles = makeStyles(theme => ({
     marginBottom: 30,
     height: 600,
     width: 600,
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       width: "100%",
       height: 300,
     },
@@ -81,20 +80,19 @@ const useStyles = makeStyles(theme => ({
   },
   figureLegend: {
     maxWidth: "70%",
-    [theme.breakpoints.down('sm')]: {
-      maxWidth: "100%"
+    [theme.breakpoints.down("sm")]: {
+      maxWidth: "100%",
     },
-  }
+  },
 }));
 
-const StyledLink = withStyles(theme => ({
+const StyledLink = withStyles((theme) => ({
   root: {
-    '&:hover': {
+    "&:hover": {
       color: "#af7b6b",
     },
-  }
-}))(props => <Link underline="none" {...props} />);
-
+  },
+}))((props) => <Link underline="none" {...props} />);
 
 function Opsr(props) {
   window.scrollTo(0, 0);
@@ -102,44 +100,61 @@ function Opsr(props) {
   const theme = useTheme();
   const classes = useStyles();
 
-  const smallWidth = useMediaQuery(theme.breakpoints.down('sm'));
+  const smallWidth = useMediaQuery(theme.breakpoints.down("sm"));
 
-  const [paperLink, paperLinkLabel] = ['https://weiphil.s3.eu-central-1.amazonaws.com/OPSR_EGSR2021.pdf', "Paper (58.8 MiB)"];
-  const [supplementalLink,supplementalLabel] = ['https://weiphil.s3.eu-central-1.amazonaws.com/opsr_supplemental.zip', "Supplemental (153.8 MB)"];
-  const [presentationLink,presentationLabel] = ['https://weiphil.s3.eu-central-1.amazonaws.com/opsr_egsr_presentation.pptx', "Presentation (40.8 MB)"];
+  const [paperLink, paperLinkLabel] = [
+    "https://weiphil.s3.eu-central-1.amazonaws.com/OPSR_EGSR2021.pdf",
+    "Paper (58.8 MiB)",
+  ];
+  const [supplementalLink, supplementalLabel] = [
+    "https://weiphil.s3.eu-central-1.amazonaws.com/opsr_supplemental.zip",
+    "Supplemental (153.8 MB)",
+  ];
+  const [presentationLink, presentationLabel] = [
+    "https://weiphil.s3.eu-central-1.amazonaws.com/opsr_egsr_presentation.pptx",
+    "Presentation (40.8 MB)",
+  ];
 
   return (
     <Container className={classes.content}>
       <div className={classes.root}>
-
         <Grid
           container
           direction="column"
           justify="flex-start"
           alignItems={smallWidth ? "flex-start" : "center"}
         >
-
-          <Grid item  >
-
+          <Grid item>
             <Typography variant="h5" gutterBottom>
-              <Box fontWeight="fontWeightRegular">Optimised Path Space Regularisation</Box>
+              <Box fontWeight="fontWeightRegular">
+                {" "}
+                Optimised Path Space Regularisation
+              </Box>
             </Typography>
 
             <Typography>
-              <StyledLink href={process.env.PUBLIC_URL + "/"}>Philippe Weier</StyledLink>, Marc Droske, Johannes Hanika, Andrea Weidlich, Jiří Vorba, 2021
+              <StyledLink href={process.env.PUBLIC_URL + "/"}>
+                Philippe Weier
+              </StyledLink>
+              , Marc Droske, Johannes Hanika, Andrea Weidlich, Jiří Vorba, 2021
             </Typography>
 
             <Typography gutterBottom>
-              Published in Computer Graphics Forum (Proceedings of <StyledLink href="https://egsr.eu/">Eurographics Symposium on Rendering</StyledLink>)
+              Published in Computer Graphics Forum (Proceedings of{" "}
+              <StyledLink href="https://egsr.eu/">
+                Eurographics Symposium on Rendering
+              </StyledLink>
+              )
             </Typography>
 
             <Card className={classes.card}>
-              <CardMedia className={classes.image}
-                image={require('../data/opsr/beach_thumbnail.png')}
+              <CardMedia
+                className={classes.image}
+                image={require("../data/opsr/beach_thumbnail.png")}
                 title="OPSR Teaser"
               />
             </Card>
-           
+
             <Grid item style={{ marginTop: 10 }}>
               <Grid
                 container
@@ -147,73 +162,114 @@ function Opsr(props) {
                 justify="flex-start"
                 alignItems="center"
               >
-
                 <Grid item>
-                  <Button variant="outlined" color="secondary" className={classes.button} target="_blank" href={paperLink}><DescriptionIcon className={classes.icon} />
+                  <Button
+                    variant="outlined"
+                    color="secondary"
+                    className={classes.button}
+                    target="_blank"
+                    href={paperLink}
+                  >
+                    <DescriptionIcon className={classes.icon} />
                     {paperLinkLabel}
                   </Button>
                 </Grid>
 
                 <Grid item>
-                  <Button variant="outlined" color="secondary" className={classes.button} target="_blank" href={supplementalLink}><ArchiveIcon className={classes.icon} />
+                  <Button
+                    variant="outlined"
+                    color="secondary"
+                    className={classes.button}
+                    target="_blank"
+                    href={supplementalLink}
+                  >
+                    <ArchiveIcon className={classes.icon} />
                     {supplementalLabel}
                   </Button>
                 </Grid>
 
                 <Grid item>
-                  <Button variant="outlined" color="secondary" className={classes.button} target="_blank" href={presentationLink}><SlideshowIcon className={classes.icon} />
+                  <Button
+                    variant="outlined"
+                    color="secondary"
+                    className={classes.button}
+                    target="_blank"
+                    href={presentationLink}
+                  >
+                    <SlideshowIcon className={classes.icon} />
                     {presentationLabel}
                   </Button>
                 </Grid>
-
               </Grid>
 
-
-              <Grid item style={smallWidth ? { marginTop: 20 } : { marginTop: 40 }} >
+              <Grid
+                item
+                style={smallWidth ? { marginTop: 20 } : { marginTop: 40 }}
+              >
                 <Typography variant="h6" gutterBottom>
                   <Box fontWeight={500}>Abstract</Box>
                 </Typography>
                 <Typography variant="body1" gutterBottom>
-                  We present Optimised Path Space Regularisation (OPSR), a novel regularisation technique for forward path tracing algorithms. Our regularisation controls the amount of roughness added to materials depending on the type of sampled paths and trades a small error in the estimator for a drastic reduction of variance in difficult paths, including indirectly visible caustics. We formulate the problem as a joint bias-variance minimisation problem and use differentiable rendering to optimise our model. The learnt parameters generalise to a large variety of scenes irrespective of their geometric complexity. The regularisation added to the underlying light transport algorithm naturally allows us to handle the problem of near-specular and glossy path chains robustly. Our method consistently improves the convergence of path tracing estimators, including state-of-the-art path guiding techniques where it enables finding otherwise hard-to-sample paths and thus, in turn, can significantly speed up the learning of guiding distributions.  
+                  We present Optimised Path Space Regularisation (OPSR), a novel
+                  regularisation technique for forward path tracing algorithms.
+                  Our regularisation controls the amount of roughness added to
+                  materials depending on the type of sampled paths and trades a
+                  small error in the estimator for a drastic reduction of
+                  variance in difficult paths, including indirectly visible
+                  caustics. We formulate the problem as a joint bias-variance
+                  minimisation problem and use differentiable rendering to
+                  optimise our model. The learnt parameters generalise to a
+                  large variety of scenes irrespective of their geometric
+                  complexity. The regularisation added to the underlying light
+                  transport algorithm naturally allows us to handle the problem
+                  of near-specular and glossy path chains robustly. Our method
+                  consistently improves the convergence of path tracing
+                  estimators, including state-of-the-art path guiding techniques
+                  where it enables finding otherwise hard-to-sample paths and
+                  thus, in turn, can significantly speed up the learning of
+                  guiding distributions.
                 </Typography>
-                
               </Grid>
 
-              <Grid item style={smallWidth ? { marginTop: 20 } : { marginTop: 40 }} >
+              <Grid
+                item
+                style={smallWidth ? { marginTop: 20 } : { marginTop: 40 }}
+              >
                 <Typography variant="h6" gutterBottom style={{ marginTop: 10 }}>
-                  <Box fontWeight={500}>
-                  Presentation
-                  </Box>
+                  <Box fontWeight={500}>Presentation</Box>
                 </Typography>
 
-                <ReactPlayer url={'https://weiphil.s3.eu-central-1.amazonaws.com/opsr_presentation_final.mp4'} controls={true} />
+                <ReactPlayer
+                  url={
+                    "https://weiphil.s3.eu-central-1.amazonaws.com/opsr_presentation_final.mp4"
+                  }
+                  controls={true}
+                />
 
-                <Typography variant="body2" gutterBottom style={{ marginTop: 10 }}> 
-                  The original EGSR talk can also be found <StyledLink href={'https://www.youtube.com/watch?v=u9HqKGqvJhQ&t=2074s'}>here</StyledLink> 
+                <Typography
+                  variant="body2"
+                  gutterBottom
+                  style={{ marginTop: 10 }}
+                >
+                  The original EGSR talk can also be found{" "}
+                  <StyledLink
+                    href={"https://www.youtube.com/watch?v=u9HqKGqvJhQ&t=2074s"}
+                  >
+                    here
+                  </StyledLink>
                 </Typography>
-                
               </Grid>
 
-                <Typography variant="h6" gutterBottom style={{ marginTop: 50 }}>
-                  <Box fontWeight={500}>More information soon!</Box>
-                </Typography>
-
-
+              <Typography variant="h6" gutterBottom style={{ marginTop: 50 }}>
+                <Box fontWeight={500}>More information soon!</Box>
+              </Typography>
             </Grid>
-
-
           </Grid>
-
-
-
-        </Grid >
-
-      </div >
-    <Footer />
-
+        </Grid>
+      </div>
+      <Footer />
     </Container>
   );
-
 }
 
 export default Opsr;
