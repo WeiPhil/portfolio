ee040593103bf4e8c88c0ecaf7b9e549eb30baa0
WeiPhil <ph.weier@gmail.com>
2025-05-10 13:52:28 +0200

    material v6 working

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,5 @@
 import { grey } from "@mui/material/colors";
-import red from "@mui/material/colors/red";
+import { red } from "@mui/material/colors";
 import { createTheme } from '@mui/material/styles';
 
 // A custom theme for this app
